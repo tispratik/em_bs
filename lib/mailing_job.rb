096a34b4648c7df0405fb50185da333502879e88
pvijayror <s.saru3@gmail.com>
2010-06-17 01:04:24 +0530

    updated view

--- lib/mailing_job.rb
+++ lib/mailing_job.rb
@@ -2,14 +2,14 @@ require 'net/smtp'
 require 'smtp_tls'
 
 
-class MailingJob < Struct.new(:user_mails)
+class MailingJob < Struct.new(:user_mails, :content)
 
   def perform
         mailer_config = File.open("#{RAILS_ROOT}/config/email.yml")
         mailer_options = YAML.load(mailer_config)
         user_mails.each do |u|
         mailing = User.find(u)
-         ActionMailer::Base.perform_delivery_smtp mailer_options , mailing.email
+         ActionMailer::Base.perform_delivery_smtp mailer_options , mailing.email, content
          mailing.deliver
        end
   end
