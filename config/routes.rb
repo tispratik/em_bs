5b934ab964e1b32b8196824f0bcb84a92cb94b4f
pvijayror <s.saru3@gmail.com>
2010-06-18 00:00:58 +0530

    routs

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,19 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :comments
 
   map.resources :wikis, :has_many => :comments
-  map.resources :tasks, :has_many => :comments, :member => { :deliver => :post }
+  #map.resources :tasks, :has_many => :comments, :member => { :deliver => :post }
 
 
-  map.resources :projects
+  map.resources :projects do |projects|
+     projects.resources :tasks, :has_many => :comments, :member => { :deliver => :post }
+  end
 
   map.signup 'signup', :controller => 'users', :action => 'new'
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
   map.login 'login', :controller => 'user_sessions', :action => 'new'
   map.resources :user_sessions
 
-  map.tasks_comment "/tasks/:id/comments", :controller => 'comments', :action => 'index'
+  #map.tasks_comment "/tasks/:id/comments", :controller => 'comments', :action => 'index'
   map.resources :users
 
   map.root :projects
