efd5906bb06ddafd01b5c91863c739f6780a49a6
pvijayror <s.saru3@gmail.com>
2010-06-28 02:03:46 +0530

    asset and task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
+require 'em_bs'
 class Task < ActiveRecord::Base
 
  belongs_to :project
@@ -7,16 +8,38 @@ class Task < ActiveRecord::Base
  validates_presence_of :name
  validates_presence_of :assign_to
 
-  def self.create_email_chain  task_users , subject
-      Delayed::Job.enqueue MailingJob.new(task_users, subject) #, 0, 4.minutes.from_now
+
+
+  def self.create_email_chain  task_users , message
+      Delayed::Job.enqueue MailingJob.new(task_users, message) #, 0, 4.minutes.from_now
   end
 
   def send_to task, comment
+      user_objects = []
       if task.is_a? Task
           assign_to = Task.find(task).assign_to.to_i
           user = User.find(assign_to)
-          Delayed::Job.enqueue MailingJob.new(user, comment) #, 0, 4.minutes.from_now
+          user_objects.push user
+           message_str :title_from =>"Task", :from => "me@fromdomain.com", :title_to =>"Assign To", :to => user.email ,
+                      :subject => "Comment" , :body => comment
+          Delayed::Job.enqueue MailingJob.new(user_objects, text_message) #, 0, 4.minutes.from_now
        end
   end
+
+
+ @@messge = nil
+ def message_str options = {}
+   @@messge = <<MESSAGE_END
+From: #{options[:title_from]} <#{options[:from]}>
+To:  #{options[:title_to]} <#{options[:to]}>
+Subject: #{options[:subject]}
+
+#{options[:body]}
+MESSAGE_END
+ end
+
+ def text_message
+    @@messge
+  end 
 
 end
