0dfba3e981fc2c10a40555c3515b85d8c993d30e
pvijayror <s.saru3@gmail.com>
2010-06-28 02:04:17 +0530

    wiki

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -5,6 +5,14 @@ class Wiki < ActiveRecord::Base
   belongs_to :project
 
    def self.create_email_chain   project_users , subject
+     message = <<MESSAGE_END
+From: Task <me@fromdomain.com>
+To: Assign to <#{project_users[0].email}>
+Subject: #{subject}
+
+This is a test e-mail message.
+MESSAGE_END
+
       Delayed::Job.enqueue MailingJob.new(project_users, subject) #, 0, 4.minutes.from_now
    end
 
