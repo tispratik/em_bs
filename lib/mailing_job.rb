eae9133afa8222d10110af387f7d492914e3de66
pvijayror <s.saru3@gmail.com>
2010-06-27 13:49:07 +0530

    em_bs

--- lib/mailing_job.rb
+++ lib/mailing_job.rb
@@ -6,14 +6,14 @@ class MailingJob < Struct.new(:user_mails, :content)
   def perform
         mailer_config = File.open("#{RAILS_ROOT}/config/email.yml")
         mailer_options = YAML.load(mailer_config)
-        if user_mails.size == 1
-           ActionMailer::Base.perform_delivery_smtp mailer_options , user_mails[0].email, content
-        else
+       # if user_mails.size == 1
+       #    ActionMailer::Base.perform_delivery_smtp mailer_options , user_mails[0].email, content
+       # else
           user_mails.each do |u|
             mailing = User.find(u)
             ActionMailer::Base.perform_delivery_smtp mailer_options , mailing.email, content
           end
          end
-      end 
+     # end
 end
 
