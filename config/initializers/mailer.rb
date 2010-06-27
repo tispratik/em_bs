c5491e8d87c60f2a466f94755cc2d3e5d5318fb7
pvijayror <s.saru3@gmail.com>
2010-06-28 02:04:54 +0530

    email.yml

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,7 +1,7 @@
 module ActionMailer
   class Base
     def self.perform_delivery_smtp(smtp_settings, destinations , content)
-
+  
       sender = "test@test.com"     
       mail = "this a test"
 
@@ -9,7 +9,7 @@ module ActionMailer
       smtp.enable_starttls_auto if smtp_settings[:enable_starttls_auto] && smtp.respond_to?(:enable_starttls_auto)
 
       smtp.start(smtp_settings[:domain], smtp_settings[:user_name], smtp_settings[:password],smtp_settings[:authentication]) do |smtp|
-           smtp.sendmail(content, sender, destinations)
+           smtp.send_message(content, sender, destinations)
       end
     end
   end
